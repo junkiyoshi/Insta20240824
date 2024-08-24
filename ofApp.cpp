28ddc99d5202280ed434dcb10498de375a804194
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2024-08-24 19:19:49 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,133 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(25);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(39);
+	ofSetLineWidth(0.5);
+	ofEnableDepthTest();
+
+	this->line.setMode(ofPrimitiveMode::OF_PRIMITIVE_LINES);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	ofSeedRandom(39);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	this->cam.begin();
+
+	float phi_deg_step = 2;
+	float theta_deg_step = 10;
+
+	float R = 250;
+	float r = R * 0.15;
+
+	for (float z = -600; z <= 600; z += 15) {
+
+		auto phi_deg = z;
+
+		this->face.clear();
+		this->line.clear();
+
+		ofPushMatrix();
+		ofTranslate(0, z, 0);
+		ofRotateY((z + 600) + ofGetFrameNum() * 2.88);
+
+		for (float theta_deg = 0; theta_deg < 360; theta_deg += theta_deg_step) {
+
+			auto index = this->face.getNumVertices();
+			vector<glm::vec3> vertices;
+
+			vertices.push_back(glm::vec3(this->make_point(R, r, theta_deg - theta_deg_step * 0.5, -phi_deg_step * 0.5)));
+			vertices.push_back(glm::vec3(this->make_point(R, r, theta_deg + theta_deg_step * 0.5, -phi_deg_step * 0.5)));
+			vertices.push_back(glm::vec3(this->make_point(R, r, theta_deg - theta_deg_step * 0.5, phi_deg_step * 0.5)));
+			vertices.push_back(glm::vec3(this->make_point(R, r, theta_deg + theta_deg_step * 0.5, phi_deg_step * 0.5)));
+
+			this->face.addVertices(vertices);
+			this->line.addVertices(vertices);
+
+			this->face.addIndex(index + 0); this->face.addIndex(index + 1); this->face.addIndex(index + 3);
+			this->face.addIndex(index + 0); this->face.addIndex(index + 3); this->face.addIndex(index + 2);
+
+			this->line.addIndex(index + 0); this->line.addIndex(index + 1);
+			this->line.addIndex(index + 2); this->line.addIndex(index + 3);
+		}
+
+		for (float theta_deg = 0; theta_deg < 360; theta_deg += theta_deg_step) {
+
+			auto index = this->face.getNumVertices();
+			vector<glm::vec3> vertices;
+
+			vertices.push_back(glm::vec3(this->make_point(R, r, theta_deg - theta_deg_step * 0.5, 180 - phi_deg_step * 0.5)));
+			vertices.push_back(glm::vec3(this->make_point(R, r, theta_deg + theta_deg_step * 0.5, 180 - phi_deg_step * 0.5)));
+			vertices.push_back(glm::vec3(this->make_point(R, r, theta_deg - theta_deg_step * 0.5, 180 + phi_deg_step * 0.5)));
+			vertices.push_back(glm::vec3(this->make_point(R, r, theta_deg + theta_deg_step * 0.5, 180 + phi_deg_step * 0.5)));
+
+			this->face.addVertices(vertices);
+			this->line.addVertices(vertices);
+
+			this->face.addIndex(index + 0); this->face.addIndex(index + 1); this->face.addIndex(index + 3);
+			this->face.addIndex(index + 0); this->face.addIndex(index + 3); this->face.addIndex(index + 2);
+
+			this->line.addIndex(index + 0); this->line.addIndex(index + 1);
+			this->line.addIndex(index + 2); this->line.addIndex(index + 3);
+		}
+
+		ofSetColor(0);
+		this->face.draw();
+
+		ofSetColor(255);
+		this->line.drawWireframe();
+
+		ofPopMatrix();
+	}
+
+	this->cam.end();
+
+	/*
+	int start = 150;
+	if (ofGetFrameNum() > start) {
+
+		ostringstream os;
+		os << setw(4) << setfill('0') << ofGetFrameNum() - start;
+		ofImage image;
+		image.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+		image.saveImage("image/cap/img_" + os.str() + ".jpg");
+		if (ofGetFrameNum() - start >= 25 * 20) {
+
+			std::exit(1);
+		}
+	}
+	*/
+}
+
+//--------------------------------------------------------------
+glm::vec3 ofApp::make_point(float R, float r, float u, float v) {
+
+	// 数学デッサン教室 描いて楽しむ数学たち　P.31
+
+	u *= DEG_TO_RAD;
+	v *= DEG_TO_RAD;
+
+	auto x = (R + r * cos(u)) * cos(v);
+	auto y = (R + r * cos(u)) * sin(v);
+	auto z = r * sin(u);
+
+	return glm::vec3(x, y, z);
+}
+
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
